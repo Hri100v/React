f7267d2dd32d9d34f6210bf7bbda55876ff6560d
Hristo Hristov <hr100h@gmail.com>
2019-02-16 16:46:54 +0200

    fetch data - update to 'App.js"

--- colorize/src/App.js
+++ colorize/src/App.js
@@ -3,6 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  status = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(new Error(response.statusText));
+    }
+  };
+
+  constructor(props) {
+    super(props);
+
+    const albumsArray = fetch("https://jsonplaceholder.typicode.com/photos", { albumId: 1 })
+      .then(this.status)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(1001, data);
+        // let result = `<h2> Random User Info From Jsonplaceholder API</h2>`;
+        // data.forEach((album) => {
+        //   const { id, albumId } = album;
+        //   if (albumId < 6) {
+        //     console.log(album, id);
+        //   }          
+        // });
+      })
+      .catch(reason => {
+        console.error(reason);
+      });
+
+    this.state = {
+      albums: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,6 +55,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <main>
+          <div id={"root"} />
+        </main>
       </div>
     );
   }
