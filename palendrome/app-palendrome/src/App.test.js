1f126a376af6e3fa6e3ef8d7ef731f9dcc9c344c
Hristo <hr100h@gmail.com>
2022-01-14 09:51:18 +0200

    Initial Testing
    These are first steps in testing with Jest.

--- palendrome/app-palendrome/src/App.test.js
+++ palendrome/app-palendrome/src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-dom/test-utils';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/* Start Testing */
+describe('Start Testing', () => {
+  test('renders initial test', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/start now/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+  test('renders the main container exists', () => {
+    // const { app } = render(<App />);
+    // const mainContainer = screen.queryAllByText('main');
+    // const mainContainer = app.firstChild();
+    // console.log(mainContainer, 1001);
+    // expect(mainContainer).toBeInTheDocument();
+
+    // const mainContainer = screen.getByRole('div');
+    // expect(mainContainer).toHaveClass('main');
+
+    ReactDOM.render(<App />);
+
+    let mainContainer = TestUtils.findRenderedDOMComponentWithClass('main');
+    expect(mainContainer).to.exists();
+  });
 });
