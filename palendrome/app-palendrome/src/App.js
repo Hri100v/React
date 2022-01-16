59db6c0199ad580842ca79174c75d336d13a0f73
Hristo <hr100h@gmail.com>
2022-01-16 15:29:16 +0200

    Additional testing cases and improvements

--- palendrome/app-palendrome/src/App.js
+++ palendrome/app-palendrome/src/App.js
@@ -1,7 +1,39 @@
+import React, { useState } from 'react';
+import './App.css';
+
 function App() {
+  let palendrome = [];
+  
+  const reverseWord = (value) => {
+    for (let i = value.length - 1; i <= 0; i -= 1) {
+      const letter = value[i];
+      palendrome.push(letter);
+    }
+  };
+
+  const handleInput = (event) => {
+    console.log('handleInput()');
+    // console.log(event);
+  };
+
+  const checkForm = () => {
+    console.log('checkForm()');
+    return 1;
+  };
+
   return (
-    <div className="main">
-      <h1>Start Now!</h1>
+    <div className='app'>
+      <div className='main'>
+        <h1>Start Now!</h1>
+        
+        <div className='inputArea'>
+          <input placeholder={'type a symetric word'} onChange={handleInput} />
+          <button onClick={checkForm}>Submit</button>
+        </div>
+
+        <div className='message'>empty</div>
+
+      </div>
     </div>
   );
 }
--- palendrome/app-palendrome/src/App.test.js
+++ palendrome/app-palendrome/src/App.test.js
@@ -1,44 +1,69 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { checkForm } from './App';
+
+// jest.mock('./App', () => { checkForm: jest.fn() });
 
 let container;
+let mainContainer;
+
+// let checkForm
 
 beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
+  // container = document.createElement('div');
+  // document.body.appendChild(container);
+
+  render(<App />);
+  mainContainer = document.getElementsByClassName('main');
 });
 
 afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
+  // document.body.removeChild(container);
+  // container = null;
 });
 
 /* Start Testing */
 describe('Start Testing', () => {
   test('renders initial test', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/start now/i);
-    expect(linkElement).toBeInTheDocument();
+    const startElement = screen.getByText(/start now/i);
+    expect(startElement).toBeInTheDocument();
   });
-  
+
   test('renders the main container exists', () => {
-    /* const { app } = */render(<App />);
-    let mainContainer = document.getElementsByClassName('main');
     expect(mainContainer.length > 0).toBe(true);
+  });
+
+  test('initial state of the input', () => {
+    expect(mainContainer[0].getElementsByTagName('input')[0].value == 0).toBe(true);
+  });
+
+  test('button name', () => {
+    expect(mainContainer[0].getElementsByTagName('button')[0].innerHTML == 'Submit').toBe(true);
+  });
+
+  test('message container exists', () => {
+    const contentMessageContainer = 'empty';
+    expect(mainContainer[0].getElementsByClassName('message')[0].textContent == contentMessageContainer);
   });
 });
 
 /* Functional Testing */
 describe('Functional Testing', () => {
-  test('click on check button', () => {
-    return false;
+  test('click on check button', () => {    
+    let submitButton = mainContainer[0].getElementsByTagName('button')[0];
+    submitButton.click();
+    expect(checkForm).toHaveBeenCalled();
   });
 
   test('get input', () => {
-    return false;
+    let input = mainContainer[0].getElementsByTagName('input')[0];
+    expect(input).toBeInTheDocument();
   });
 
   test('matcher function', () => {
+    App.checkForm = jest.fn( _ => { return 2 });
+    expect(App.checkForm()).toBe(2);
+
     return false;
   });
 
