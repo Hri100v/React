5ddbb7b106b9d68a5246a8f31700c2a1a45d3b02
Hristo <hr100h@gmail.com>
2022-07-13 10:14:58 +0300

    Clearing Folders
    Some folders were not updated and they may be refactored in the future

--- snake/src/game/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { GameSnake } from './game';
-
-// export const Game = (props) => {
-//     return <Game width={900} height={1200}></Game>
-// };
-
-export default { Game: GameSnake };
