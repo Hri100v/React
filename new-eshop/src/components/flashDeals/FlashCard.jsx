089dbb18ee09665c722f3de07fd0876a41ac93cb
Hristo Hristov <hr100h@gmail.com>
2025-02-01 08:24:13 +0200

    Start adding a testing scenario

--- new-eshop/src/components/flashDeals/FlashCard.jsx
+++ new-eshop/src/components/flashDeals/FlashCard.jsx
@@ -34,8 +34,35 @@ export const FlashCard = ({ productItems, addToCard }) => {
     infinite: true,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    // slidesToScroll: 1,
+    slidesToScroll: 4,
     autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ],
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
   };
